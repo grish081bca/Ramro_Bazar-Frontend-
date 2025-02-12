71012af8274e63ec733eccd0223ee7bd2d6dc40c
Grish Shrestha <grishshrestha2@gmail.com>
2025-02-12 16:34:09 +0545

    Updated UI and fetched APi

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,24 +1,41 @@
+import { ProductDetails } from './../api-response';
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CrudService } from '../crud-operations/crud.service';
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.scss'
 })
-export class AddProductComponent {
-  @Output() productAdded = new EventEmitter<any>();
 
-  newProduct = { name: '', price: null, category: '', image: '' };
+export class AddProductComponent {
+  productDetail : ProductDetails = {
+    productId: 0,  // Assuming it's auto-generated in the backend
+    name: '',
+    description: '',
+    price: 0,
+    brand: '',
+    category: '',
+    releaseDate: '',
+    available: false,
+    quantity: 0,
+  };
+
+  constructor(private crudService : CrudService){}
 
   addProduct() {
-    if (this.newProduct.name && this.newProduct.price && this.newProduct.category && this.newProduct.image) {
-      this.productAdded.emit(this.newProduct);
-      this.newProduct = { name: '', price: null, category: '', image: '' };
-    } else {
-      alert('Please fill all fields');
+    if(this.productDetail){
+      this.crudService.addProducts(this.productDetail).subscribe((res)=>{
+        if(res.ok){
+          alert("Added Product Successfully")
+          window.location.reload();
+        }else{
+          alert("Failed To Add Product");
+        }
+      })
     }
   }
 }
