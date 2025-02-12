1bcc9d757b4916fe33683190937b689544901614
Grish Shrestha <grishshrestha2@gmail.com>
2025-02-12 16:34:57 +0545

    fetched more enpoints api from backend to frontend

--- src/app/crud-operations/crud.service.ts
+++ src/app/crud-operations/crud.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api.service';
-import { ProductDetailsResponse } from '../api-response';
+import { ProductDetails, ProductDetailsResponse } from '../api-response';
+import { Observable } from 'rxjs';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,17 @@ export class CrudService {
     return this.apiservice.get<ProductDetailsResponse>('api/products');
   }
 
-  getProductById(){
-    return this.apiservice.get<ProductDetailsResponse>('api/products');
+  getProductById(productId : number){
+    return this.apiservice.get<ProductDetailsResponse>('api/products/' + productId) ;
   }
 
+  addProducts(
+    addProduct : ProductDetails
+  ): Observable<HttpResponse<ProductDetailsResponse>> {
+    return this.apiservice.post('api/add-product', addProduct);
+  }
+
+  deleteProduct(productId : number){
+    return this.apiservice.post('api/delete/products/' + productId);
+  }
 }
